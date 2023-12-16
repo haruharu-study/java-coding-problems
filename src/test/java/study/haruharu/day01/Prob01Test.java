031a0957677e935e0e5541e62f9127051a74b934
honki12345 <iui4793@gmail.com>
2023-12-16 14:30:59 +0900

    test: pull request

--- src/test/java/study/haruharu/day01/Prob01Test.java
+++ src/test/java/study/haruharu/day01/Prob01Test.java
@@ -2,6 +2,19 @@ package study.haruharu.day01;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class Prob01Test {
+    @DisplayName("")
+    @Test
+    void test() {
+        // given
+
+        // when
+
+        // then
+
+    }
 
 }
